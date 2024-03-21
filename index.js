de28cc569f47ee8c75c76cde8780f560939dc5cf
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-03-21 09:09:02 +0100

    Use file:// URL for puppeteer

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import api from './lib/api.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(serveStatic('dist'));
-
 api(app);
 
 app.listen(port, () => console.log(`ol-tileserver listening on port ${port}.`));
